e7f682e654df90f411709d18a39f103b3e815c12
Bobby <gizmo91200@yahoo.fr>
2026-03-25 15:00:42 +0100

    ajout commentaire pour to do list

--- src/api/cart/read.php
+++ src/api/cart/read.php
@@ -1,3 +1,31 @@
 <?php
+
+include __DIR__ . '/../../../includes/db.php';
+session_start();
+$user_id = $_SESSION['user_id'] ?? null;
+
+if (!$user_id ) {
+    header('Location: ../../shopping.php');
+    exit;
+}
+
+if ($user_id) {
+    $stmt = $pdo->prepare("
+        SELECT cart_items.*, products.name_product, products.price_product, products.image_path 
+        FROM cart_items 
+        JOIN products ON cart_items.id_product = products.id_product 
+        WHERE cart_items.id_user = ?
+    ");
+    $stmt->execute([$user_id]);
+    $items = $stmt->fetchAll();
+    
+    $_SESSION['cart'] = $items;
+    header('Location: ../../cart.php');
+    exit;
+}
+
+header('Location: ../../cart.php');
+exit;
 // TODO: ajout code pour localStorage (non-connecté)
+
 ?>
--- src/api/cart/update.php
+++ src/api/cart/update.php
@@ -1,3 +1,23 @@
 <?php
+
+include __DIR__ . '/../../../includes/db.php';
+session_start();
+$user_id = $_SESSION['user_id'] ?? null;
+$product_id = $_POST['product_id'] ?? null;
+$qty = $_POST['qty'] ?? null;
+
+if ($product_id) {
+    $stmt = $pdo->prepare("UPDATE users SET $qty= ? WHERE user_id= ? ");
+    $stmt->execute([$user_id]);
+    $items = $stmt->fetchAll();
+    
+    $_SESSION['cart'] = $items;
+    header('Location: ../../cart.php');
+    exit;
+}
+
+header('Location: ../../cart.php');
+exit;
 // TODO: ajout code pour localStorage (non-connecté)
+
 ?>
