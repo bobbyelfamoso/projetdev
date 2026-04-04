0c1510a32bb5d1ba6754723256ae8c4fd73942e2
Bobby <gizmo91200@yahoo.fr>
2026-04-04 20:48:05 +0200

    animation addtocart pdescription

--- src/js/product.js
+++ src/js/product.js
@@ -11,3 +11,10 @@ if (imageSection && imageImg) {
         imageImg.style.transform = 'scale(1)';
     });
 }
+
+let bouton = document.documentElementbyClassName('.add-to-cart-btn');
+let texteInitial = bouton.textContent;
+
+bouton.addEventListener("mouseover", () => { bouton.style.background = "#9ccc65"; bouton.textContent = "I WANT IT!"; });
+
+bouton.addEventListener("mouseout", () => { bouton.style.background = "#2e4f21"; bouton.textContent = texteInitial; });
